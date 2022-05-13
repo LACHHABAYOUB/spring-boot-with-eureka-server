560ffa7503b402e020705e06fa2bd582e17fab49
LACHHAB AYOUB <44031876+LACHHABAYOUB@users.noreply.github.com>
2022-05-13 18:07:11 -0500

    first commit

---
+++ PhotoAppApiUsers/PhotoAppApiUsers/src/test/java/com/example/api/users/PhotoAppApiUsers/PhotoAppApiUsersApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.api.users.PhotoAppApiUsers;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PhotoAppApiUsersApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
---
+++ PhotoAppService/PhotoAppService/src/test/java/com/example/eureka/PhotoAppService/PhotoAppServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.eureka.PhotoAppService;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PhotoAppServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
